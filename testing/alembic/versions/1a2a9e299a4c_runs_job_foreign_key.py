a9be8b7c2b6d52754fe616ea4dc4c240ab9c009f
Cesar Roux Dit Buisson <cesar.roux-dit-buisson13@imperial.ac.uk>
2015-07-15 18:28:10 +0100

    Versioning with filling of job_id in test_runs

--- testing/alembic/versions/1a2a9e299a4c_runs_job_foreign_key.py
+++ testing/alembic/versions/1a2a9e299a4c_runs_job_foreign_key.py
@@ -22,6 +22,8 @@ def upgrade():
 
     op.add_column('test_runs', sa.Column('job_id', sa.Integer(), nullable=True))
     op.create_foreign_key('test_runs_job_id_fkey', 'test_runs', 'test_jobs', ['job_id'], ['id'], source_schema='cr1013', referent_schema='cr1013')
+    # Run an update to import values of job_id into new foregin key column.
+    op.execute('update cr1013.test_runs set job_id = cr1013.test_batches.job_id from cr1013.test_batches where cr1013.test_batches.id = batch_id;')
     ### end Alembic commands ###
 
 
