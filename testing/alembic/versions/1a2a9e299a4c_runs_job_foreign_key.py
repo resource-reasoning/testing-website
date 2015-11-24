9cc4e67ac227756cdbf17cd80f65707a0d14a4d3
Thomas Wood <tw1509@doc.ic.ac.uk>
2015-11-24 19:40:28 +0000

    Missing schema on migration

--- testing/alembic/versions/1a2a9e299a4c_runs_job_foreign_key.py
+++ testing/alembic/versions/1a2a9e299a4c_runs_job_foreign_key.py
@@ -20,7 +20,7 @@ def upgrade():
     
     ### Upgrade to version that contains job_id in Runs directly. ###
 
-    op.add_column('test_runs', sa.Column('job_id', sa.Integer(), nullable=True))
+    op.add_column('test_runs', sa.Column('job_id', sa.Integer(), nullable=True), schema='cr1013')
     op.create_foreign_key('test_runs_job_id_fkey', 'test_runs', 'test_jobs', ['job_id'], ['id'], source_schema='cr1013', referent_schema='cr1013')
     # Run an update to import values of job_id into new foregin key column.
     op.execute('update cr1013.test_runs set job_id = cr1013.test_batches.job_id from cr1013.test_batches where cr1013.test_batches.id = batch_id;')
@@ -29,5 +29,5 @@ def upgrade():
 
 def downgrade():
     op.drop_constraint('test_runs_job_id_fkey', 'test_runs', schema='cr1013', type_='foreignkey')
-    op.drop_column('test_runs', 'job_id')
+    op.drop_column('test_runs', 'job_id', schema='cr1013')
     ### end Alembic commands ###
