9e01b878f883416ca14b0f11dc86c7e12b662e44
Thomas Wood <thomas.wood09@imperial.ac.uk>
2015-12-07 02:04:42 +0000

    es6 helper

--- testing/helpers.py
+++ testing/helpers.py
@@ -1,6 +1,10 @@
 # Helper functions for rendering engines
 def test262_link(path):
-    base_url = "https://github.com/tc39/test262/tree/es5-tests/"
     (_, _, subpath) = path.rpartition('test262/')
+    if subpath:
+        base_url = "https://github.com/tc39/test262/tree/es5-tests/"
+    else:
+        (_, _, subpath) = path.rpartition('test262-es6/')
+        base_url = "https://github.com/tc39/test262/tree/master/"
     return base_url + subpath
 
